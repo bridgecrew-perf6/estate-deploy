ffe317477bd5e85fb60f5756387fd82da8f43628
Ishtiak Ahmed <iahmed1606@gmail.com>
2022-01-16 20:35:17 +0600

    copy message added

--- apps/components/Footer/Footer.tsx
+++ apps/components/Footer/Footer.tsx
@@ -4,6 +4,10 @@ const Footer = () => {
 	return (
 		<footer style={{ padding: '1em', textAlign: 'center' }}>
 			<p>All rights reserved by Sweet Home 2021-</p>
+			<p>
+				Developed by{' '}
+				<a href='https://www.linkedin.com/in/ishtiak-aahmed/'>Ishtiak Ahmed</a>
+			</p>
 		</footer>
 	);
 };
